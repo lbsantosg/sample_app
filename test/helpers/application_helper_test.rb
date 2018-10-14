43b513d900519b8f3ba1cc2abdb6212989a22c7a
Laura Santos <lbsantosg@unal.edu.co>
2018-10-14 00:38:47 -0500

    Finish layout and routes

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+  base_title = "Ruby on Rails Tutorial Sample App"
+  test "full title helper" do
+    assert_equal full_title,(base_title)
+    assert_equal full_title("Help"), ("Help | #{base_title}")
+  end
+  test "full title signup" do
+    assert_equal full_title, (base_title)
+    assert_equal full_title("Sign up"), ("Sign up | #{base_title}")
+  end
+end
