396a3672d237043e722ef8e852c533e67321f1ec
Laura Santos <lbsantosg@unal.edu.co>
2018-10-14 22:15:19 -0500

    Change test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,4 @@
 ENV['RAILS_ENV'] ||= 'test'
-ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 require "minitest/reporters"
@@ -8,6 +7,6 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-  include ApplicationHelper
+	include ApplicationHelper
   # Add more helper methods to be used by all tests here...
 end
